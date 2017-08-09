73ecc4fe1500e7a29f83d8543fff3cd625915411
Boris Doesborg <bdoesborg@gmail.com>
2017-08-09 23:41:39 +0200

    thumbnail

---
+++ configure-env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Configuring local development environment for local.site..."
+
+# Change directory to the webroot.
+cd /var/www/privsites/cristina/web
+
+# Sanitize the database.
+drush sql-sanitize -y
+
+# Disable modules.
+#drush dis -y ldap_authentication
+
+# Enable modules.
+drush en devel -y
+drush en devel_generate -y
+#drush en reroute_email -y
+drush en stage_file_proxy -y
+drush en environment_indicator -y
+
+# Clear Drupal caches.
+drush cc all
+
+echo "Environment configuration complete."
