50fb205300cc792a6cf2fdec6d19e2fe436a596e
Boris Doesborg <bdoesborg@gmail.com>
2017-11-10 15:54:34 +0100

    disable dev modules

--- configure-env.sh
+++ configure-env.sh
@@ -5,8 +5,10 @@ echo "Configuring local development environment for local.site..."
 # Change directory to the webroot.
 cd /var/www/cristina/web
 
+chmod -R 777 sites/default
+
 # Sanitize the database.
-drush sql-sanitize -y
+# drush sql-sanitize -y
 
 # Disable modules.
 #drush dis -y ldap_authentication
@@ -14,6 +16,7 @@ drush sql-sanitize -y
 # Enable modules.
 drush en devel -y
 drush en devel_generate -y
+drush en kint -y
 #drush en reroute_email -y
 drush en stage_file_proxy -y
 drush en environment_indicator -y
