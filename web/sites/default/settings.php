65e0cae95f1790431a1d8b32d92082e64f1ef252
Boris Doesborg <bdoesborg@gmail.com>
2018-03-31 20:43:04 +0200

    d8.5

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -750,9 +750,18 @@ $settings['file_scan_ignore_directories'] = [
  * Keep this code block at the end of this file to take full effect.
  */
 $config['config_split.config_split.config_dev']['status'] = FALSE;
+$settings['install_profile'] = 'minimal';
+$config_directories['sync'] = '../config/sync';
+$settings['trusted_host_patterns'] = array(
+  '^www\.cristinasilvan\.com$',
+  '^.+\.cristinasilvan\.com$',
+  '^\.cristinasilvan\.com$',
+  '^cri\.doesb\.org$',
+  '^cri-build\.doesb\.org$',
+  '^cri\.drupalvm\.test$',
+  '^cri\.kem$',
+);
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
-$settings['install_profile'] = 'minimal';
-$config_directories['sync'] = '../config/sync';
 
