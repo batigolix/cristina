ba0d6958a5ed75be68357169e6f5cc6371672653
Boris Doesborg <bdoesborg@gmail.com>
2021-02-12 21:48:52 +0100

    wip

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -753,17 +753,6 @@ $config['config_split.config_split.config_dev']['status'] = FALSE;
 
 $config_directories['sync'] = '../config/sync';
 
-$settings['trusted_host_patterns'] = array(
-  '^www\.cristinasilvan\.com$',
-  '^.+\.cristinasilvan\.com$',
-  '^\.cristinasilvan\.com$',
-  '^cristinasilvan\.com$',
-  '^cristina\.test$',
-  '^cristina\.docksal$',
-  '^cristina-build\.test$',
-  '^cristina-stage\.doesb\.org$',
-);
-
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
